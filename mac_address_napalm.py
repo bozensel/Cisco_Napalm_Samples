9cbb1e37354fca9c937a0fc6020f53b5851fe312
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-06-07 12:40:31 +0300

    Create mac_address_napalm.py

---
+++ mac_address_napalm.py
@@ -0,0 +1,12 @@
+# MAC ADDRESS
+
+import json
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+
+iosvl2 = driver('10.10.10.10', 'baris', 'cisco')
+iosvl2.open()
+
+ios_output = iosvl2.get_mac_address_table()
+print (json.dumps(ios_output, indent=4))
